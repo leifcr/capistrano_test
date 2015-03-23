4df69d27e37c33668a72ab12bc5d03714828db2e
Leif Ringstad <leifcr@gmail.com>
2015-03-23 15:57:40 +0100

    link logs and bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,7 @@ set :pty, true # Must be enabled for sudo'ing
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
